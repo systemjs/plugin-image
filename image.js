c45eae361530ddca948f974d715fbb34c8663087
Guy Bedford <guybedford@gmail.com>
2014-01-29 23:18:54 +0200

    Update image.js

--- image.js
+++ image.js
@@ -7,13 +7,13 @@ module.exports = function(name, address, fetch, callback, errback) {
   img = new Image();
   img.onerror = errback;
   img.onload = function(evt) {
+    try {
+      delete img.onload; //release memory - suggested by John Hann
+    } catch(err) {
+      img.onload = function() {}; // IE7 :(
+    }
     setTimeout(function() {
       callback('module.exports = new Image();\nmodule.exports.src = "' + address + '";');
-      try {
-        delete img.onload; //release memory - suggested by John Hann
-      } catch(err) {
-        img.onload = function() {}; // IE7 :(
-      }
     }, 7);
   }
   img.src = address;
