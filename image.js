c3484e4a3ff840bcc9b477e7fa6671ec6bd8d956
Guy Bedford <guybedford@gmail.com>
2014-01-29 23:27:17 +0200

    move to passing same img object for bad caches

--- image.js
+++ image.js
@@ -3,18 +3,18 @@
   Adapted from Miller Medeiros requirejs-plugins
   https://github.com/millermedeiros/requirejs-plugins/blob/master/src/image.js
 */
-module.exports = function(name, address, fetch, callback, errback) {    
+module.exports = function(name, address, fetch, callback, errback) {
   img = new Image();
   img.onerror = errback;
   img.onload = function(evt) {
+    window.__imagePluginStore.push(img);
+    callback('module.exports = window.__imagePluginStore[' + window.__imagePluginStore.length - 1 + '];');
     try {
       delete img.onload; //release memory - suggested by John Hann
     } catch(err) {
       img.onload = function() {}; // IE7 :(
     }
-    setTimeout(function() {
-      callback('module.exports = new Image();\nmodule.exports.src = "' + address + '";');
-    }, 7);
   }
   img.src = address;
 }
+window.__imagePluginStore = [];
