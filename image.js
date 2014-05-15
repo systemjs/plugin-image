040ad373e88c176b9c4dff812ae0c9c78970363a
guybedford <guybedford@gmail.com>
2014-05-14 17:43:37 -0700

    new plugin format

--- image.js
+++ image.js
@@ -3,18 +3,22 @@
   Adapted from Miller Medeiros requirejs-plugins
   https://github.com/millermedeiros/requirejs-plugins/blob/master/src/image.js
 */
-module.exports = function(name, address, fetch, callback, errback) {
-  var img = new Image();
-  img.onerror = errback;
-  img.onload = function(evt) {
-    window.__imagePluginStore.push(img);
-    callback('module.exports = window.__imagePluginStore[' + (window.__imagePluginStore.length - 1) + '];');
-    try {
-      delete img.onload; //release memory - suggested by John Hann
-    } catch(err) {
-      img.onload = function() {}; // IE7 :(
+exports.fetch = function(load) {
+  return new Promise(function(resolve, reject) {
+    var img = load.metadata.img = new Image();
+    img.onerror = reject;
+    img.onload = function(evt) {      
+      try {
+        delete img.onload; //release memory - suggested by John Hann
+      } catch(err) {
+        img.onload = function() {}; // IE7 :(
+      }
+      resolve('');
     }
-  }
-  img.src = address;
-}
-window.__imagePluginStore = [];
+    img.src = load.address;
+  });
+};
+
+exports.instantiate = function(load) {
+  return load.metadata.img;
+};
